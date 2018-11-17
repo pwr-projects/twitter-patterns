b1a27a89ee4a1bd3d8e6bceb0df8e29d6794f18f
suchygrzech <suszka.grzegorz@gmail.com>
2018-11-17 01:37:09 +0100

    add additional flag to preprocessing

--- dataset/_utils.py
+++ dataset/_utils.py
@@ -164,7 +164,7 @@ def only_lang_users(nicks_csv_path: str, output_path: str=None, lang: str ='en')
     return lang_users
 
 
-def preprocess_tweets(tweets: Sequence[str], lemma:bool=True, remove_url:bool=True) -> Sequence[str]:
+def preprocess_tweets(tweets: Sequence[str], lemma:bool=True, remove_url:bool=True, remove_mentions:bool=True) -> Sequence[str]:
     lemmatizer = nltk.stem.WordNetLemmatizer()
     preprocessed_tweets = []
     for tweet in tqdm(tweets, desc='preprocessing tweets'):
@@ -173,9 +173,10 @@ def preprocess_tweets(tweets: Sequence[str], lemma:bool=True, remove_url:bool=Tr
         tweet = str(tweet).translate(str.maketrans({key: None for key in string.punctuation}))
         tweet = nltk.word_tokenize(tweet)
         tweet = map(lambda word: word.lower(), tweet)
-        tweet = filter(lambda word: not word.startswith('@'), tweet)
+        tweet = filter(lambda word: not word.startswith('@'), tweet) if remove_mentions else Tweet
         tweet = map(lemmatizer.lemmatize, tqdm(tweet, 'Lemmatizing')) if lemma else tweet
         tweet = ' '.join(tweet)
         if tweet:
             preprocessed_tweets.append(tweet)
     return preprocessed_tweets
+
