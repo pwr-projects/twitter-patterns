1caa707794fef31fa39e19460357452fbc8a55b7
suchygrzech <suszka.grzegorz@gmail.com>
2018-12-17 00:06:23 +0100

    fiiiiix

--- dataset/utils.py
+++ dataset/utils.py
@@ -16,20 +16,23 @@ from tqdm import tqdm
 
 from config import *
 
-__all__ = [
-    'get_batch',
-    'summary_dict',
-    'find_duplicates',
-    'get_extra_user_from_hashtags',
-    'to_lower',
-    'preprocess_tweets',
-    'wc'
-]
+from .scrappers import *
+
+nltk.download('punkt')
 
 
 def wc(filename):
     return int(check_output(['wc', '-l', filename]).split()[0])
 
+
+def load_userlist_from_file(filename: str, filepath: str = USERLISTS_HELPERS_DIR):
+    with open(os.path.join(filepath, filename), 'r') as f:
+        users = f.readlines()
+    users = map(lambda user: user.replace('\n', ''), users)
+    return set(users)
+
+def get_group_tweets(groupname:str) -> pd.DataFrame:
+    return pd.read_csv(os.path.join(TWEETS_DIR, groupname, TWEETS_FILENAME), header=0)
 
 def get_batch(groups, group_name, batch_size: int, batch_idx: int):
     data = list(groups[group_name])
@@ -61,69 +64,29 @@ def to_lower(strs: Sequence['str']):
     return list(map(lambda elem: elem.lower(), strs))
 
 
-def download_hashtags(*hashtags: Sequence[str], tweets_limit: int = 10000):
-    if os.path.isdir(HASHTAG_DIR):
-        shutil.rmtree(HASHTAG_DIR)
-    config = twint.Config()
-    config.Search = ' '.join('#' + hashtag for hashtag in hashtags)
-    config.Lang = 'en'
-    config.Store_csv = True
-    config.Output = HASHTAG_DIR
-    config.User_full = True
-    config.Followers = True
-    config.Limit = tweets_limit
-    config.Lowercase = True
-    config.Verified = True
-    twint.run.Search(config)
-
-
-def scrap_tweets(user: str, group: str, limit: int = 100000):
-    if user is None:
-        return
-    config = twint.Config()
-    config.Username = user
-    config.Limit = limit
-    config.Output = os.path.join(TWEETS_DIR, group)
-    config.Store_csv = True
-    config.Lang = 'en'
-    config.Hide_output = False
-    config.Format = '{id}'
-    twint.run.Search(config)
-
-
-def download_followers(username: str, usergroup: str = '', function=twint.run.Followers):
-    config = twint.Config()
-    config.Username = username
-    config.Lang = 'en'
-    config.Store_csv = True
-    config.Output = os.path.join(FOLLOWERS_DIR, usergroup, username)
-    config.Lowercase = True
-    function(config)
-
-
 def extract_tweet_authors(tweets_dir: str) -> Sequence[str]:
     content = pd.read_csv(os.path.join(tweets_dir, TWEETS_FILENAME), header=0)
     return content.username.unique()
 
 
-def get_users_with_lower_bound(users_dir: str, min_followers: int = 10000) -> Sequence[str]:
+def get_users_with_min_followers_no(users_dir: str, min_followers: int) -> Sequence[str]:
     assert min_followers > 0, 'min_followers should be greater than 0'
     content = pd.read_csv(os.path.join(users_dir, USERS_FILENAME), header=0)
     content = content[['username', 'followers']].drop_duplicates('username')
     return to_lower(content[content.followers > min_followers].username)
 
 
-def find_duplicates(groups: Dict[str, Sequence[str]], other_users: Sequence[str] = list()) -> Set[str]:
+def get_duplicates_in_dict(groups: Dict[str, Sequence[str]], other_users: Sequence[str] = list()) -> Set[str]:
     return set(k for k, v in Counter(chain(*groups.values(), other_users)).items() if v > 1)
 
 
 def add_not_duplicates(groups: Dict[str, Set[str]], category: str, users: Sequence[str]):
-    to_add = set(users) - find_duplicates(groups, users)
+    to_add = set(users) - get_duplicates_in_dict(groups, users)
     groups[category].update(to_add)
     return groups
 
 
-def create_group_dict_from_userlists(*categories: Sequence[str]) -> Dict[str, Set[str]]:
+def create_group_dict_from_userlists(*categories: Sequence[str], userlist_dir: str = USERS_LIST_DIR) -> Dict[str, Set[str]]:
     groups = {}
     for category in tqdm(categories, 'Reading group file'):
         with open(os.path.join(USERS_LIST_DIR, f'{category}.txt'), mode='r') as f:
@@ -148,7 +111,7 @@ def export_users_dict_to_files(groups: Dict[str, Set[str]], outdir: str = TEMP_D
 
 
 def merge_with_scraped(group_name: str, other_users: Sequence['str']):
-    groups = create_group_dict_from_userlists(group_name)
+    groups = create_group_dict_from_userlists(group_name, userlist_dir=RAW_USERLISTS_FROM_PAGE)
     groups = add_not_duplicates(groups, group_name, other_users)
     export_users_dict_to_files(groups)
     return groups
@@ -198,7 +161,8 @@ def preprocess_tweets(tweets: Sequence[str], lemma: bool = True, remove_url: boo
     preprocessed_tweets = []
     for tweet in tqdm(tweets, desc='preprocessing tweets'):
         tweet = tweet.replace('#', '')
-        tweet = re.sub(r'http\S+', '', tweet) if remove_url else tweet
+        tweet = re.sub(r'(?:(?:(?:www|http)[:/]*)*\S+(?:\.(?:com|org|pl))(?:/\S+)*)',
+                       '', tweet) if remove_url else tweet
         tweet = str(tweet).translate(str.maketrans({key: None for key in string.punctuation}))
         tweet = nltk.word_tokenize(tweet)
         tweet = map(lambda word: word.lower(), tweet)
@@ -210,10 +174,27 @@ def preprocess_tweets(tweets: Sequence[str], lemma: bool = True, remove_url: boo
     return preprocessed_tweets
 
 
-def get_extra_user_from_hashtags(group_name: str, low_followers_bound: int, *hashtags: Sequence[str]):
+def get_extra_users_from_hashtags(group_name: str, low_followers_bound: int, *hashtags: Sequence[str]):
     download_hashtags(hashtags, tweets_limit=100000)
 
     for user in tqdm(extract_tweet_authors(HASHTAG_DIR), 'Downloading users followers'):
         download_followers(user)
 
-    merge_with_scraped(group_name, get_users_with_lower_bound(FOLLOWERS_DIR, min_followers=low_followers_bound))
+    merge_with_scraped(group_name, get_users_with_min_followers_no(FOLLOWERS_DIR, min_followers=low_followers_bound))
+
+
+def get_tweets_with_filtered_users(group: str, tweet_count_threshold: int):
+    print('Reading tweet file...')
+    tweets = pd.read_csv(os.path.join(TWEETS_DIR, group, TWEETS_FILENAME),
+                         low_memory=False,
+                         header=0)
+
+    print('Filtering users...')
+    filtered_users = tweets.groupby('username')['id'].count().reset_index(name='count')
+    filtered_users = filtered_users.sort_values('count')
+    filtered_users = filtered_users[filtered_users['count'] > tweet_count_threshold]
+
+    print('Filtering tweets...')
+    tweets = tweets[tweets.username.isin(filtered_users.username)]
+
+    return tweets, filtered_users
